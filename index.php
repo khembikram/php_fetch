b3535646caaac2a0f17b56340ebd3e8c218f739a
Sujan Rana <Khembikram1@gmail.com>
2023-10-23 09:09:23 +0545

    commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fetch functions in php</title>
+    <style>
+        table {
+            border:  1px solid rgb(217, 217, 217);
+        }
+        tr,th{
+            padding:  8px;
+        }
+        th {
+            border-left: 1px solid rgb(217, 217, 217);
+        }
+        tr:nth-child(even){
+            background-color: rgb(94, 115, 179);
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Date of Birth</th>
+            <th>Email Id</th>
+            <th>Mobile</th>
+            <th>Photo</th>
+        </tr>
+    </table>
+</body>
+</html>
